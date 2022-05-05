5437cd04d9ecf0ae1032644072f03cbb561243fa
Kizomba <kizomba@github.com>
2022-05-05 13:49:34 +0200

    v05

--- wiki_page_function.py
+++ wiki_page_function.py
@@ -17,8 +17,12 @@ def get_wiki_page_existence(wiki_page_url, timeout=10):
 
 wiki_page_urls = ["https://en.wikipedia.org/wiki/" + str(i) for i in range(50)]
 
-print("Running without threads:")
-without_threads_start = time.time()
-for url in wiki_page_urls:
-    print(get_wiki_page_existence(wiki_page_url=url))
-print("Without threads time:", time.time() - without_threads_start)
+print("Running threaded:")
+threaded_start = time.time()
+with concurrent.futures.ThreadPoolExecutor() as executor:
+    futures = []
+    for url in wiki_page_urls:
+        futures.append(executor.submit(get_wiki_page_existence, wiki_page_url=url))
+    for future in concurrent.futures.as_completed(futures):
+        print(future.result())
+print("Threaded time:", time.time() - threaded_start)
